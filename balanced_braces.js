0f52fac8906d7bea474803b528dd7c6b995c327a
Blickhan <JB035624@cerner.net>
2018-02-22 11:18:18 -0600

    modified balanced braces to allow any character with an opening/closing pair to be easily added

--- balanced_braces.js
+++ balanced_braces.js
@@ -7,34 +7,20 @@ function braces(values) {
 }
 
 function isBalanced (str) {
-    let stack = [];    
+    let stack = [];
+    const opening_chars = ['(','{','['];
+    const closing_chars = [')','}',']']; // closing chars must be at the same index as its respective opening char
     for(char of str) {
-        if(char == '(' || char == '{' || char == '[') {
+        if(opening_chars.indexOf(char) > -1) { // if this char is an opening char
             stack.push(char);
         }
-        else if(char == ')') {
-            if(stack[stack.length - 1] == '(') {
+        else if(closing_chars.indexOf(char) > -1) { // if this char is a closing char
+            if(stack[stack.length - 1] == opening_chars[closing_chars.indexOf(char)]) {
                 stack.pop();
             }
             else {
                 return false;
             }            
-        }
-        else if(char == '}') {
-            if(stack[stack.length - 1] == '{') {
-                stack.pop();
-            }
-            else {
-                return false;
-            }   
-        }
-        else if(char == ']') {
-            if(stack[stack.length - 1] == '[') {
-                stack.pop();
-            }
-            else {
-                return false;
-            }   
         }
     }
     return stack.length == 0;
